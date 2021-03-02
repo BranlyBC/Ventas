de887ab333d42067e75455d60df25abd98bdcc78
Bily Fernandez <cursosdecomputacion.net@gmail.com>
2021-03-01 21:19:16 -0600

    actualizando tarea

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -7,19 +7,3 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace Ventas
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show(" Ningún mar en calma hizo experto a un marinero ");
-        }
-    }
-}
