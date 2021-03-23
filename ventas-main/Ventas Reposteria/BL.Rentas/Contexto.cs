98341c4e29e117b3c289875f08e07c21cb070301
Branly Flores <branly.flores@unah.hn>
2021-03-23 11:46:32 -0600

    actualizacion

---
+++ ventas-main/Ventas Reposteria/BL.Rentas/Contexto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Rentas
+{
+     public class Contexto: DbContext
+    {
+        public Contexto(): base("VideoJuegos")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            Database.SetInitializer(new DatosdeInicio()); // Agrega datos de inicio
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tipo> Tipos { get; set; }
+        public object Usuarios { get; internal set; }
+    }
+}
