90af9ba5deb1ef48e5a30f36b06bffc38f5a1553
Branly Flores <branly.flores@unah.hn>
2021-03-24 21:27:27 -0600

    42

--- ventas-main/Ventas Reposteria/BL.Rentas/FacturaBL.cs
+++ ventas-main/Ventas Reposteria/BL.Rentas/FacturaBL.cs
@@ -67,10 +67,31 @@ namespace BL.Rentas
                 return resultado;
             }
 
+            CalcularExistencia(factura);
+            
 
             _contexto.SaveChanges();
             resultado.Exitoso = true;
             return resultado;
+        }
+
+        private void CalcularExistencia(Factura factura)
+        {
+            foreach (var detalle in factura.FacturaDetalle)
+            {
+                var producto = _contexto.Productos.Find(detalle.ProductoId);
+                if (producto != null)
+                {
+                    if (factura.Activo == true)
+                    {
+                        producto.Existencia = producto.Existencia - detalle.Cantidad;
+                    }
+                    else
+                    {
+                        producto.Existencia = producto.Existencia + detalle.Cantidad;
+                    }
+                }
+            }
         }
 
 
@@ -157,6 +178,9 @@ namespace BL.Rentas
                 if (factura.Id == id)
                 {
                     factura.Activo = false;
+
+                    CalcularExistencia(factura);
+
                     _contexto.SaveChanges();
                     return true;
                 }
